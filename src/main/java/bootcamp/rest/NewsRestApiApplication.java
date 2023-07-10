5a65239451765f29939212304b23ebb7ceb85726
wclouds19 <wan.essentials@gmail.com>
2023-07-10 22:04:24 +0700

    Rename Main File

--- src/main/java/bootcamp/rest/NewsRestApiApplication.java
+++ src/main/java/bootcamp/rest/NewsRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectRestApiApplication {
+public class NewsRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectRestApiApplication.class, args);
+		SpringApplication.run(NewsRestApiApplication.class, args);
 	}
 
 }
